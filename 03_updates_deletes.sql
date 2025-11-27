1847248dda535d127f1312ec61477a749c25cbd3
natanael1cirino <natanael1cirino@gmail.com>
2025-11-27 16:28:23 -0300

    atualiza e exclui

--- 03_updates_deletes.sql
+++ 03_updates_deletes.sql
@@ -1,1 +1,23 @@
 
+UPDATE cliente
+SET email = 'ana.nova@gmail.com'
+WHERE id_cliente = 1;
+
+UPDATE produto
+SET estoque = estoque + 5
+WHERE id_produto = 1;
+
+UPDATE pedido
+SET status = 'ENVIADO'
+WHERE id_pedido = 1;
+
+DELETE FROM pagamento
+WHERE forma_pagamento = 'BOLETO';
+
+
+DELETE FROM item_pedido
+WHERE id_pedido = 2 AND id_produto = 3;
+
+
+DELETE FROM cliente
+WHERE id_cliente = 3;
